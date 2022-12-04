796dbeb996173391b6c7d63a5e81811ab7586a6c
mrinnnmoy <mrinmoy202000@gmail.com>
2022-12-04 13:32:50 +0530

    add script-preLoader

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const loader = document.querySelector(".loader");
+
+window.addEventListener("load", () => {
+  // Making 5s delay in website loading
+  setTimeout(() => {
+    loader.classList.add("fadeOut");
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500);
+  }, 2500);
+});
